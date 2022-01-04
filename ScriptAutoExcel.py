0070f465b3b078938ab17b3247e7dc99bcaf3f33
Sebastiaan Bellinkx <sebastiaan.bellinkx@protonmail.com>
2022-01-04 22:17:49 +0100

    Update voor map te lezen

--- ScriptAutoExcel.py
+++ ScriptAutoExcel.py
@@ -1,9 +1,18 @@
 from openpyxl import Workbook
+import os
+import pandas as pd 
+cwd = os.path.abspath('') 
+files = os.listdir(cwd) 
 
-workbook = Workbook()
-sheet = workbook.active
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
 
-sheet["A1"] = "hello"
-sheet["B1"] = "world!"
+df = pd.DataFrame()
+for file in files: 
+    if file.endswith(".xlsx"):
+        df = df.append(pd.read_excel(file), ignore_index=True)
+df.Head()
 
-workbook.save(filename="hello_world.xlsx")
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+print(filename)
